1de0eb63b1f0b6f8503230cead566b36e66714c1
raymond gitonga <raytosh95@gmail.com>
2019-04-22 00:24:04 +0300

    create tests to ensure the getter methods for name and phone number in style class instantiate correctly

--- src/test/java/Salon/StylistTest.java
+++ src/test/java/Salon/StylistTest.java
@@ -6,7 +6,18 @@ public class StylistTest {
 
     @Test
     public void category_instantiatesCorrectly_true() {
-        Stylist testStylist = new Stylist("Tosh");
+        Stylist testStylist = new Stylist("Tosh","1234");
         assertEquals(true, testStylist instanceof Stylist);
+    }
+    @Test
+    public void getName_stylistInstantiatesWithName_String() {
+        Stylist testStylist = new Stylist("Tosh","1234");
+        assertEquals("Tosh", testStylist.getName());
+    }
+
+    @Test
+    public void getphoneNumber_stylistInstantiatesWithphoneNumber_String() {
+        Stylist testStylist = new Stylist("Tosh","12345");
+        assertEquals("12345", testStylist.getPhoneNumber());
     }
 }
